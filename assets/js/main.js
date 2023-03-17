8d105a020f020ddd344d1b4e54542409fa9b86f3
xGIANOx <studioenricogian@gmail.com>
2023-03-17 16:41:03 +0100

    [fix] bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,11 @@
 // Creo una variabile dove salvo il container delle celle 
 const playButton = document.querySelector(".rounded-3.p-0.px-2");
+const testSelect = document.getElementById('difficulty');
+const container = document.querySelector(".container");
 
 playButton.addEventListener("click", function() {
-  const container = document.querySelector(".container");
-
-  let cell_number = 100;
-
-  for (let i = 1; i <= cell_number; i++) {
-    const cell = `<div class="cell">${i}</div>`;
-    container.innerHTML += cell;
-  }
-
-  const cellEl = document.querySelectorAll(".cell");
-
-  for (let i = 0; i < cellEl.length; i++) {
-    const thisCell = cellEl[i];
-    console.log(thisCell);
-    thisCell.addEventListener("click", function() {
-        thisCell.classList.toggle("bg_blue");
-        console.log(i + 1);    
-    })
-  }
-});
-
-// difficulty 
-
-const testSelect = document.getElementById('difficulty');
-testSelect.addEventListener('change', function(){
-	
-	if (this.value == 'diff_1') {
+    container.innerHTML = "";
+    if (testSelect.value == 'diff_1') {
         const container = document.querySelector(".container");
 
         let cell_number = 100;
@@ -48,7 +25,7 @@ testSelect.addEventListener('change', function(){
             console.log(i + 1);    
         })
         }
-    } else if (this.value == 'diff_2'){
+    } else if (testSelect.value == 'diff_2'){
         const container = document.querySelector(".container");
 
         let cell_number = 81;
@@ -89,5 +66,5 @@ testSelect.addEventListener('change', function(){
         })
         }
     }
-    
-})
+});
+
